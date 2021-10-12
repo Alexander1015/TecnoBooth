9d2d371dac465c54f74f3ff3b2b181807696de18
BassMan14 <bryanarmandoss@hotmail.com>
2021-10-11 19:18:32 -0600

    Diseño NewGroup

--- components/screens/dashboard/CreateGroup.js
+++ components/screens/dashboard/CreateGroup.js
@@ -1,17 +1,134 @@
-import React from "react";
-import {  View, Text, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import * as ImagePicker from 'expo-image-picker';
+import { stylesnewgroup } from "../../../resources/styles/StyleNewGroup";
+import { View, Text, ScrollView, Platform, TouchableOpacity, Image, TextInput } from "react-native";
 import Styles from "../../../resources/styles/Dashboard";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import { useForm, Controller } from "react-hook-form";
+
+const schema = yup.object({
+    nombre: yup.string().required("El nombre es obligatorio"),
+    descripcion: yup.string().required("La descripcion es obligatoria")
+})
 
 const CreateGroupScreen = (route) => {
     const { navigation } = route;
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    });
+    const [image, setImage] = useState(null);
+    useEffect(() => {
+        (async () => {
+            if (Platform.OS !== 'web') {
+                const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+                if (status !== 'granted') {
+                    alert('Sorry, we need camera roll permissions to make this work!');
+                }
+            }
+        })();
+    }, []);
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
 
-    return(
-        <View style={ Styles.container }>
-            <ScrollView vertical>
-                <Text style={ Styles.texto }>Estamos en Dashboard</Text>
-                <Text style={ Styles.texto }>Soy Crear tu propio Grupo</Text>
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
+    const submit =(data)=> {console.log(data)}
+
+    return (
+        <View style={stylesnewgroup.container}>
+            <ScrollView style={{flex: 1}} vertical>
+                
+                <View style={stylesnewgroup.contenedorImagen}>
+                    <TouchableOpacity style={stylesnewgroup.botonImagen} onPress={pickImage}>
+                        <Text style={stylesnewgroup.textoImagen}>Agregar imagen</Text>
+                    </TouchableOpacity>
+                    {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+                </View>
+                <View style={stylesnewgroup.containerForm}>
+                        <View style={stylesnewgroup.viewInput}>
+                        <Text style={stylesnewgroup.textInput}>
+                            Agregar nombre
+                        </Text>
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                style={stylesnewgroup.input}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
+                        name="nombre"
+                        defaultValue=""
+                    />
+                    {errors.nombre && <Text style={stylesnewgroup.textoError}>{errors.nombre.message}</Text>}
+                    </View>
+            
+               
+                        <View style={stylesnewgroup.viewInput}>
+                        <Text style={stylesnewgroup.textInput}>
+                            Agregar descripcion
+                        </Text>
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                multiline
+                                style={[stylesnewgroup.input, {height: 100}]}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
+                        name="descripcion"
+                        defaultValue=""
+                    />
+                     {errors.descripcion && <Text style={stylesnewgroup.textoError}>{errors.descripcion.message}</Text>}
+                    </View>
+                    
+                   
+                
+          
+                        <View style={stylesnewgroup.viewInput}>
+                        <Text style={stylesnewgroup.textInput}>
+                            Otra informacion
+                        </Text>
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                multiline
+                                style={[stylesnewgroup.input, {height: 100}]}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
+                        name="otraInfo"
+                        defaultValue=""
+                    />
+                    </View>
+                    
+                </View>
+                <TouchableOpacity style={stylesnewgroup.botonGuardar} onPress={handleSubmit(submit)}>
+                        <Text style={stylesnewgroup.textoImagen}>Guardar grupo</Text>
+                    </TouchableOpacity>
             </ScrollView>
+
         </View>
+
     );
 }
 
