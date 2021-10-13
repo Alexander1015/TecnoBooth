b65c3d6c040f8ab92d123d95f7c212c5cda4923b
BassMan14 <79945146+BassMan14@users.noreply.github.com>
2021-10-13 11:52:37 -0600

    Solucionando errores (#3)

--- components/screens/dashboard/CreateGroup.js
+++ components/screens/dashboard/CreateGroup.js
@@ -2,8 +2,44 @@ import React from "react";
 import {  View, Text, ScrollView } from "react-native";
 import Styles from "../../../resources/styles/Dashboard";
 
-const CreateGroupScreen = ( props ) => {
-    const { userEmail } = props;
+
+const schema = yup.object({
+    nombre: yup.string().required("El nombre es obligatorio"),
+    descripcion: yup.string().required("La descripcion es obligatoria")
+})
+
+const CreateGroupScreen = (route) => {
+    const { navigation } = route;
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    });
+    const [image, setImage] = useState(null);
+    useEffect(() => {
+        (async () => {
+            if (Platform.OS !== 'web') {
+                const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+                if (status !== 'granted') {
+                    alert('Sorry, we need camera roll permissions to make this work!');
+                }
+            }
+        })();
+    }, []);
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
+
 
     return(
         <View style={ Styles.container }>
