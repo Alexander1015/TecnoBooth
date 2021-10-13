1ffb0a9154ea89ef9710c0e6b3c7d6d1dc3e2eaa
luiscotoo <luiscoto001@gmail.com>
2021-10-13 16:13:16 -0600

    Agregué pantalla de Info-grupos

---
+++ components/screens/dashboard/Grupos/screens/Integ.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import { Card } from 'react-native-paper';
+export default function Info() {
+    return (
+    <ScrollView>
+      <View>
+        <Text>Aquí es Integrantes</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
