3d3add83e63ea15397fd9068bd920806b8c5a9a8
Luis Coto <42676379+luiscotoo@users.noreply.github.com>
2021-11-07 20:13:34 -0600

    Modificacion post y validaciones imagen (#18)
    * Unión Firebase-Pantalla
    
    * Seleccion individual grupos
    
    * Pantalla integrantes (grupos)
    
    * Adaptacion con Home
    
    * Diseño de Actividad
    
    * Diseño y prototipo actividad
    
    * Modificacion Actividad
    
    * Cambio diseño posts y validaciones imagenes.

--- resources/styles/styleHome.js
+++ resources/styles/styleHome.js
@@ -21,14 +21,12 @@ export const styleHome = StyleSheet.create({
     img:{
       width: 300,
       height: 300,
-
     },
     contenedorOpciones:{
       // flexDirection:'row',
       paddingVertical:10,
       paddingHorizontal:15,
       alignItems:'center',
-      marginVertical:15
     },
     nombre:{
       fontSize:40,
@@ -42,10 +40,65 @@ export const styleHome = StyleSheet.create({
       borderRadius:10,
       marginHorizontal:10,
     },
+
+    btnsuscribe:{
+      flexDirection: "row",
+      paddingHorizontal:5,
+      marginTop:5,
+      paddingVertical:10,
+      backgroundColor:'#969C9F',
+      borderRadius:10,
+      marginHorizontal:10,
+    },
     btnText:{
       color:'white',
       fontSize:16
-    }
+    },
+    hr: {
+      borderColor: Colors.SECUNDARY_COLOR,
+      borderBottomWidth: 1,
+      width: "90%",
+  },
+  grpbtn: {
+      marginTop: 5,
+  },
+  btn2: {
+      backgroundColor: Colors.SECUNDARY_COLOR,
+      width: "100%",
+      padding: 5,
+      borderRadius: 5,
+  },
+  btndecorado: {
+      margin: 1,
+      borderColor: Colors.PRIMARY_COLOR,
+      borderWidth: 1,
+      padding: 5,
+  },
+  txtbtn: {
+      color: Colors.PRIMARY_COLOR,
+      fontSize: 15,
+      textAlign: "center",
+      fontWeight: "bold",
+  },
+  viewbtndecor: {
+    marginTop:15,
+    width: "90%",
+},
+texto: {
+  color: Colors.SECUNDARY_COLOR,
+  fontSize: 28,
+  textAlign: "center",
+},
+card_template: {
+  marginBottom: 20,
+},
+card_image: {
+  alignSelf: "center",
+  width: 300,
+  height: 200,
+  borderRadius: 10,
+  marginTop: 30,
+},
 })
 
 
