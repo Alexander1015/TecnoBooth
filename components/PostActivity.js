a7a757b9ed16b14189b0e32aebd8d09b0a0aec97
Alexander1015 <alexanderbarrera105@gmail.com>
2021-11-07 19:21:24 -0600

    Se arreglo la pantalla de Actividad, para que muestre los post por grupos

---
+++ components/PostActivity.js
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from "react";
+import {
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    TouchableOpacity,
+} from "react-native";
+import useGrupos from "../hooks/useGrupos";
+import Comentario from "./Comentario";
+
+const Post = ({ post, verificado, idUsuario }) => {
+    const { descripcion, img, usuario, id } = post;
+
+    const { obtenerComentarios, comentarios } = useGrupos();
+
+    const [verComentario, setVerComentario] = useState(false);
+
+    useEffect(() => {
+        if (verComentario) {
+            obtenerComentarios(id);
+        }
+    }, [verComentario]);
+    return (
+        <View style={styles.container}>
+            <Text style={styles.usuario}>{usuario}</Text>
+            <View style={styles.imgContainer}>
+                <Image style={styles.img} source={{ uri: img }} />
+            </View>
+            <View style={styles.info}>
+                <Text style={styles.texto}> {descripcion} </Text>
+            </View>
+            <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.btnComentario}
+                onPress={() => setVerComentario(!verComentario)}
+            >
+                <Text style={[styles.texto, { color: "white" }]}>
+                    {verComentario ? "Ocultar Comentarios" : "Ver Comentarios"}
+                </Text>
+            </TouchableOpacity>
+            {verComentario && comentarios ? (
+                <View style={{ backgroundColor: "#252527" }}>
+                    {comentarios.length > 0 &&
+                        comentarios.map((comentario) => (
+                            <View key={comentario.id}>
+                                <Comentario comentarioUsuario={comentario} />
+                            </View>
+                        ))}
+                </View>
+            ) : null}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#DBE5EA",
+        margin: 15,
+        borderRadius: 10,
+        padding: 10,
+    },
+    imgContainer: {
+        alignItems: "center",
+        paddingTop: 25,
+    },
+    img: {
+        width: 200,
+        height: 200,
+    },
+    texto: {
+        fontSize: 18,
+    },
+    usuario: {
+        position: "absolute",
+        top: 5,
+        left: 10,
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+    info: {
+        marginVertical: 10,
+    },
+    btnComentario: {
+        marginVertical: 10,
+        paddingHorizontal: 5,
+        paddingVertical: 10,
+        backgroundColor: "#041F2E",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10,
+        marginHorizontal: 10,
+    },
+    input: {
+        paddingHorizontal: 5,
+        paddingVertical: 10,
+        fontSize: 16,
+        margin: 5,
+        backgroundColor: "white",
+        borderRadius: 20,
+    },
+});
+
+export default Post;
